7c811690e7a14fad7674d9ad879d968740c34c65
elayne.lemos <elayne.lemos@sysvale.com>
2023-02-26 16:54:33 -0300

    variables

--- config/settings/production.py
+++ config/settings/production.py
@@ -125,6 +125,12 @@ EMAIL_BACKEND = env(
     default="django.core.mail.backends.smtp.EmailBackend",
 )
 
+EMAIL_HOST = env('EMAIL_HOST', '')
+EMAIL_PORT = env('EMAIL_PORT', '')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = bool(env('EMAIL_USE_TLS', ''))
+
 # Collectfast
 # ------------------------------------------------------------------------------
 # https://github.com/antonagestam/collectfast#installation
