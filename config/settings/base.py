79a9d1696a2651e9a2036d5a60a586d93e89169e
elayne.lemos <elayne.lemos@sysvale.com>
2023-02-26 06:10:02 -0300

    feat: increment config

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,7 +59,7 @@ DJANGO_APPS = [
     "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "django.contrib.humanize", # Handy template tags
+    "django.contrib.humanize", # Handy template tags
     "django.contrib.admin",
     "django.forms",
 ]
@@ -73,7 +73,6 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "academy_drive.users",
-    # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
