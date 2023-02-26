b7c5f25b96f6edb1007bdc22510149d6932e98f2
elayne.lemos <elayne.lemos@sysvale.com>
2023-02-26 16:57:22 -0300

    fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -125,11 +125,11 @@ EMAIL_BACKEND = env(
     default="django.core.mail.backends.smtp.EmailBackend",
 )
 
-EMAIL_HOST = env('EMAIL_HOST', '')
-EMAIL_PORT = env('EMAIL_PORT', '')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', '')
-EMAIL_USE_TLS = bool(env('EMAIL_USE_TLS', ''))
+EMAIL_HOST = env('EMAIL_HOST', default=None)
+EMAIL_PORT = env('EMAIL_PORT', default=None)
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default=None)
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default=None)
+EMAIL_USE_TLS = env('EMAIL_USE_TLS', default=False)
 
 # Collectfast
 # ------------------------------------------------------------------------------
